ba0ad0ffc9f016e1942c44f7bc739ca05aebcd63
Michał Szymański <voiceofthunder0108@gmail.com>
2023-02-12 22:53:22 +0100

    Add variables

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-function printMessage(msg) {
-    let div = document.createElement('div');
-    div.innerHTML = msg;
-    document.getElementById('messages').appendChild(div);
-}
+let computerMove = `kamień`;
+let playerMove = `papier`;
 
-function clearMessages() {
-    document.getElementById('messages').innerHTML = '';
-}
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
